ac8e2ce6304b9d405e5ea23e3d8f42f48868d6c1
Kuura Omakone <kuuho24@gmail.com>
2025-04-28 14:53:54 +0300

    kirjahaun alkua

---
+++ javascript/suodattimet.js
@@ -0,0 +1,11 @@
+const suodattimetButton = document.querySelector(".suodattimet-button")
+const suodattimet = document.querySelector(".suodattimet")
+
+suodattimetButton.addEventListener("click", () => {
+    if(document.getElementById("suodattimet").style.display !== "flex") {
+    document.getElementById("suodattimet").style.display = "flex";
+    document.getElementById("suodattimet").style.justifyContent = "center";
+    } else {
+        document.getElementById("suodattimet").style.display = "none";
+    }
+})
