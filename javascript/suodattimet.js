ac9351e854d340ef2512f92ff846b2696fdddb4a
Kuura Omakone <kuuho24@gmail.com>
2025-05-12 11:32:54 +0300

    Hintasuodatin

--- javascript/suodattimet.js
+++ javascript/suodattimet.js
@@ -2,9 +2,10 @@ const suodattimetButton = document.querySelector(".suodattimet-button")
 const suodattimet = document.querySelector(".suodattimet")
 
 suodattimetButton.addEventListener("click", () => {
-    if(document.getElementById("suodattimet").style.display !== "flex") {
-    document.getElementById("suodattimet").style.display = "flex";
-    document.getElementById("suodattimet").style.justifyContent = "center";
+    if (document.getElementById("suodattimet").style.display !== "flex") {
+        document.getElementById("suodattimet").style.display = "flex";
+        document.getElementById("suodattimet").style.justifyContent = "center";
+        //document.getElementById("suodattimet").style.alignItems = "center";
     } else {
         document.getElementById("suodattimet").style.display = "none";
     }
